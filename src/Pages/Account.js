c44ac38bd2b708b19f89b60876d240d78674ce19
Arichson <arichsonh@gmail.com>
2021-04-22 18:05:02 -0700

    added some stuff

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -1,11 +1,50 @@
-
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
 export default function Account(props){
+    const fetchProfileInfo = async () => {    
+    
+        // turn this into something for this project
+        // try {
+        //   const response = await fetch(
+        //     "https://todos-by-bernier.herokuapp.com/todos/",
+        //     {
+        //       method: "POST",
+        //       headers: {
+        //         "Content-Type": "application/json"
+        //       },
+        //       body: JSON.stringify(newTodo)
+        //     }
+        //   );
+        //   const data = await response.json();
+        //   setTodos([...todos, data]);
+        //   setNewTodo({
+        //     subject: "",
+        //     details: ""
+        //   });
+        // } catch (error) {
+        //   console.error(error);
+        // }
+    }
+
 
     return (
         <div className="App">
             <h1>{props.page}</h1>
-
-            <h3> View and change settings for the account </h3>
+            <h3> Name, Last </h3>
+            <div>
+                <ul>
+                    <li>Location</li>
+                    <li>Email</li>
+                    <li>LinkedIn</li>        
+                    <li>Gender</li>                                       
+                    <li>Pronouns</li>                                         
+                    <li>Birthday</li>
+                </ul>
+            </div>
+            <div>
+                <h4>About Me</h4>
+                <p>Information about me</p>
+            </div>
         </div>
     )
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -52,19 +52,19 @@ function Home() {
   );
 
   // else {
-//   return (
-//     <div className="App">
-//       <h1>CODR</h1>
-//         <div>
-//             <h2>LOGIN</h2>
-//             <form>
-//                   Username: <input type='text'></input> <br />
-//                   <button type='submit' onClick={handleSubmit()}>Submit!</button>
-//             </form>
-//             <div> Don't have a login? <Link to={"/createUser"}>Create a new user!</Link> </div>
-//         </div>
-//     </div>
-//   );
+  // return (
+  //   <div className="App">
+  //     <h1>CODR</h1>
+  //       <div>
+  //           <h2>LOGIN</h2>
+  //           <form>
+  //                 Username: <input type='text'></input> <br />
+  //                 <button type='submit' onClick={handleSubmit()}>Submit!</button>
+  //           </form>
+  //           <div> Don't have a login? <Link to={"/createUser"}>Create a new user!</Link> </div>
+  //       </div>
+  //   </div>
+  // );
 }
 
 export default Home;
