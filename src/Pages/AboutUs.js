bbb2ee2a00e1d1187e728c73bdf06341b6e41f5e
bunmeev <bunmee.vang@gmail.com>
2021-04-26 05:21:24 -0400

    More CSS to FAQ and AboutMe

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -3,6 +3,7 @@ import styles from "./AboutUs.module.css";
 function AboutUs(props) {
   return (
   <div className={styles.background}>
+    <h1 className={styles.title}>About CODR</h1>
     <div className={styles.App}>
       <h1 className={styles.aboutUs}>{props.page}</h1>
       <p className={styles.intro}>We are software engineering students at General Assembly. Our goal is to make an App that people can connect to each other. </p>
